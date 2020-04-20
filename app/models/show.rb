aa826cf20b2a025304f7650214f4734c0741500b
Mark A Guza <guzamaguza@gmail.com>
2020-04-20 03:42:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,16 @@
 class Show < ActiveRecord::Base
+  #associations
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+
+  def actors_list
+    actors_array = []
+    self.characters.each do |ch|
+      name_and_char = "#{ch.name} - #{ch.show.name}"
+      role_array << name_and_char
+    end
+    return actors_array
+  end
 
 end
