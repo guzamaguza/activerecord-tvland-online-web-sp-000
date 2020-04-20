6882dfb8759159a53edd524e04810422cf2241eb
Mark A Guza <guzamaguza@gmail.com>
2020-04-20 03:47:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
 
   def actors_list
     actors_array = []
-    self.characters.each do |ch|
-      name_and_char = "#{ch.name} - #{ch.show.name}"
+    self.actors.each do |ac|
+       = "#{ac.name} - #{ac.show.name}"
       role_array << name_and_char
     end
     return actors_array
